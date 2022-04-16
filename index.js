4006ce44a0f6ba164a1639fc9f389680f15c7daa
ibluehoodie <42532938+ibluehoodie@users.noreply.github.com>
2022-04-16 13:24:57 -0400

    Remove original http.createServer listener

--- index.js
+++ index.js
@@ -250,12 +250,3 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
   console.log('Your app is running on port 8080.');
 });
-
-// const http = require('http');
-
-// http.createServer((request, response) => {
-//   response.writeHead(200, {'Content-Type': 'text/plain'});
-//   response.end('Welcome to my movie club!\n');
-// }).listen(8080);
-//
-// console.log('My first Node test server is running on Port 8080.');
