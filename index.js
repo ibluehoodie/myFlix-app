442813393054755316c366ca09609da83967bf35
ibluehoodie <42532938+ibluehoodie@users.noreply.github.com>
2022-04-16 13:17:23 -0400

    .use body-parser package with JSON

--- index.js
+++ index.js
@@ -7,22 +7,7 @@ const express = require('express'),
 
 app.use(morgan('common'));
 
-//GET requests
-app.get('/', (req, res) => {
-  res.send('Welcome to my movie club!');
-});
-//same result as:
-// response.writeHead(200, {'Content-Type': 'text/plain'});
-// response.end('Welcome to my book club!\n');
-
-//structure = app.METHOD(PATH, HANDLER) PATH = endpoint URL, HANDLER = function to be executed when route is matched;
-app.get('/documentation', (req, res) => {
-  res.sendFile('public/documentation.html', {root: __dirname });
-});
-
-app.get('/movies', (req, res) => {
-  res.json(topMovies);
-});
+app.use(bodyParser.json());
 
 //use express.static to route all requests for static files to corresponding files within selected folder (public);
 app.use(express.static('public'));
