ba72a13b0a98729db2d0038f2e601ba9af956b14
ibluehoodie <42532938+ibluehoodie@users.noreply.github.com>
2022-05-09 21:45:15 -0400

    Remove /login endpoint console.log test

--- auth.js
+++ auth.js
@@ -16,7 +16,6 @@ let generateJWTToken = (user) => {
 // POST login.
 module.exports = (router) => {
   router.post('/login', (req, res) => {
-    console.log(req.body) //test
     passport.authenticate('local', { session: false },
   (error, user, info) => {
     if (error || !user) {
