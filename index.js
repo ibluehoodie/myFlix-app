f7e10791f470bf13b6524259422a793f3f30ad80
ibluehoodie <42532938+ibluehoodie@users.noreply.github.com>
2022-05-17 23:18:31 -0400

    3.4 allow middleware w app.use(cors());

--- index.js
+++ index.js
@@ -42,18 +42,18 @@ app.use((err, req, res, next) => {
 const cors = require('cors');
 app.use(cors());
 // Define allowed domains for cors.
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed origins.
-    let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+//
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed origins.
+//     let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 // import auth.js file.
 let auth = require('./auth')(app); // passing (app) argument makes Express available in auth.js.
